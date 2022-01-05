98f4236267246a9411ee86d2f9ad3fa7778b6056
Justin <jtremblay@microsoft.com>
2022-01-05 10:32:10 -0500

    more work

--- SpicyLang/spicylang/spicyvm.cpp
+++ SpicyLang/spicylang/spicyvm.cpp
@@ -1,7 +1,66 @@
 #include "spicyvm.h"
 
+#include <iostream>
+#include <format>
+
 namespace spicy {
+    SpicyVM::SpicyVM(bool trace_execution) 
+        : trace_execution(trace_execution) {}
+    
     void SpicyVM::disassemble(const Chunk& chunk) {
         chunk.disassemble("TODO");
+    }
+    
+    // TODO: return type for status?
+    void SpicyVM::execute(const Chunk& chunk) {
+        reset();
+        
+        while (program_counter < chunk.getBytecodeCount()) {
+            if (trace_execution) {
+                printStack();
+                auto discarded = chunk.disassembleInstruction(program_counter);
+            }
+            switch (static_cast<Chunk::OpCode>(readByte(chunk))) {
+            case Chunk::OpCode::OP_CONSTANT: {
+                const auto& constants = chunk.getConstants();
+                const auto& obj = constants[readByte(chunk)];
+                push(obj);
+                break;
+            }
+            case Chunk::OpCode::OP_RETURN: {
+                std::cout << getObjString(pop()) << '\n';
+                return;
+            }
+            }
+        }
+    }
+    
+    void SpicyVM::reset() {
+        stack.clear();
+        current_stack = 0l;
+        program_counter = 0l;
+    }
+    
+    uint8_t SpicyVM::readByte(const Chunk& chunk) {
+		const auto& code = chunk.getBytecode();
+        return code[program_counter++];
+    }
+    
+    void SpicyVM::push(SpicyObj value) {
+        stack[current_stack].emplace_back(std::move(value));
+    }
+    
+    SpicyObj SpicyVM::pop() {
+        const auto value = stack[current_stack].back();
+        stack[current_stack].pop_back();
+        return value;
+    }
+    
+    std::string SpicyVM::printStack() {
+        std::cout << '\t';
+        for (const auto& value : stack[current_stack]) {
+            std::cout << std::format("[ {} ]", getObjString(value));
+        }
+        std::cout << '\n';
     }
 }
--- SpicyLang/spicylang/spicyvm.h
+++ SpicyLang/spicylang/spicyvm.h
@@ -12,11 +12,22 @@ template<typename T>
 using Stack = std::vector<T>;
     
 class SpicyVM {
+    bool trace_execution;
     Stack<Stack<SpicyObj>> stack;
+    unsigned long current_stack = 0l;
     unsigned long program_counter = 0l;
 public:
+    explicit SpicyVM(bool trace_execution);
     void disassemble(const Chunk& chunk);
     void execute(const Chunk& chunk);
+private:
+    void reset();
+    [[nodiscard]] uint8_t readByte(const Chunk& chunk);
+    
+    void push(SpicyObj value);
+    SpicyObj pop();
+    
+    std::string printStack();
 };
 
 }// namespace spicy
